abc410744bb78cf60ac68485d4ffbfb1e1c826b4
Ishany-coder <ishanghosh0106@gmail.com>
2025-02-21 16:42:24 -0800

    now code makes an excel doc and saves it onto my computer

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	optimizeDeps: {
-		include: ['googleapis'],
-	},
-	ssr: {
-		noExternal: ['googleapis']
-	}
 });
